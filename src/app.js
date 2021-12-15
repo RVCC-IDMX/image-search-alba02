4114b908a7d414d4f7848428604e337dea581bc9
alba02 <naomimaldo02@gmail.com>
2021-12-15 02:34:02 -0500

    Add images

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const form = document.querySelector('.search-form');
+const container = document.querySelector('.container');
+const template = document.getElementById('template');
+
 
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
@@ -14,6 +17,12 @@ form.addEventListener('submit', async (event) => {
     .then((res) => res.json())
     .catch((err) => console.error(err));
 
+
+
+
+
+
+
   /*
   some sample code
     const dataObj = response.results[0];
@@ -27,15 +36,41 @@ form.addEventListener('submit', async (event) => {
     template and append it to the DOM element with the .container class.
   */
 
+  response.results.forEach((element)=>{
+
+    let clone = template.content.cloneNode(true);
+
+    const dataObj = response.results[0];
+    const postImg = clone.querySelector('.post__img');
+    postImg.src = dataObj.urls.small;
+    postImg.alt = dataObj.alt_description;
+
   /*
     Add an attribution statement below the image using the
     postUser element and the photographer's name from dataObj
    */
+   const user = clone.querySelector('.post__user'); ;
+   user.innerText = element.user.name;
 
-  /*
+
+
+ /*
     Check the description of the post. If it's bot bull and less than 100 characters,
     add the description from dataObj to the post. If it's more than 100 characters,
     add the first 100 characters of the description from dataObj to the post followed by
     an ellipsis (...)
   */
+     const postDesc = clone.querySelector('.post__desc');
+    let description = dataObj.description;
+
+    if(description !== null && description.length>100){
+      description = description.substring(0,97) +'...'
+      description.innerText = description;
+    }
+
+
+
+
+container.appendChild(clone)
+  })
 });
